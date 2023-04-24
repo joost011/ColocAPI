76296005932e647638dee12f274b24ad9c5c9b5d
Joost Bakker <jo.m.bakker@st.hanze.nl>
2023-04-24 17:18:10 +0200

    Add route for file view

--- coloc_api/urls.py
+++ coloc_api/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+# todo/todo/urls.py : Main urls.py
+from django.urls import path, include
+from .views import (
+    FileView,
+)
+
+urlpatterns = [
+    path('file', FileView.as_view()),
+]
