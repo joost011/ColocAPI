de5f4fb66132746c75b1665baccf28757f64b95e
Joost Bakker <jo.m.bakker@st.hanze.nl>
2023-04-24 12:34:24 +0200

    Add FileView

--- coloc_api/views.py
+++ coloc_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.core.files.storage import default_storage
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+import json
+import uuid
 
-# Create your views here.
+class FileView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        file = request.data['file']
+        file_extension = str(file).split('.')[-1]
+        file_name = f'{uuid.uuid4()}.{file_extension}'
+        print('file', file_name)
+        default_storage.save(f'files/in_files/{file_name}', file)
+        return Response(json.loads(json.dumps({'file_name': file_name})), status=status.HTTP_200_OK)
