85d3f5bc4e008f8b5df2fc3bc9b83f9b14842079
JobMaathuis <j.maathuis@st.hanze.nl>
2023-05-15 14:26:49 +0200

    Removed redundant $ sign in coloc R script

--- scripts/coloc.R
+++ scripts/coloc.R
@@ -19,7 +19,7 @@ data$eqtls$type <- 'quant'
 # Perform colocalization
 res <- coloc.abf(dataset1=data$gwas, dataset2=data$eqtls)
 
-datasets <- list(datasets = list(meta_data = data$meta_data, gwas = data$gwas, eqtls = data$eqtls, coloc = res$results$[,c("snp","SNP.PP.H4")], posterior = res$summary))
+datasets <- list(datasets = list(meta_data = data$meta_data, gwas = data$gwas, eqtls = data$eqtls, coloc = res$results[,c("snp","SNP.PP.H4")], posterior = res$summary))
 
 if (res$summary[6] >= 0.8){
   write(toJSON(datasets), paste("storage/out_files/", file_id, "/", gene, ".json", sep=''))
