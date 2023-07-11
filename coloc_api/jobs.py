f1a7da32761877d546d74fb4e4af9c0b5269a52b
Anna van den Broek <a.h.van.den.broek@st.hanze.nl>
2023-07-11 13:51:13 +0200

    Implement cc/quant option

--- coloc_api/jobs.py
+++ coloc_api/jobs.py
@@ -2,7 +2,7 @@ from django_rq import job
 from .services.ColocService import ColocService
 
 @job('high')
-def initialize_coloc(file_name):
+def initialize_coloc(file_name, coloc_type, *args):
     '''Function that starts a coloc process on the Redis server'''
     coloc_service = ColocService()
-    coloc_service.start_coloc(file_name)
+    coloc_service.start_coloc(file_name, coloc_type, *args)
